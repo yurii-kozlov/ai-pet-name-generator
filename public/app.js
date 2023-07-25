ad7e34cc33c9475a92d81d2e72597b26142d68df
Yurii Kozlov <yurka22870@gmail.com>
2023-07-25 15:34:43 +0300

    logging into console has been removed

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('sw.js')
-    .then(() => console.log('service worker installed'))
+    .then()
     .catch((err) => console.error('Error', err));
 }
